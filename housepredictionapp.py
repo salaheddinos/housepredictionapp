eb5f342d94f017c54d3a498d7d0b1204597b8931
salah <salah.bouzdouz@gmail.com>
2022-05-22 09:16:09 +0100

    commit

--- housepredictionapp.py
+++ housepredictionapp.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import time
-import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
